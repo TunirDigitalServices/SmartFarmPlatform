860ca5045164a75e5c7ecd9611251a1fb7260257
RanyaELOUNI <raniaelounidev@gmail.com>
2025-06-09 11:46:06 +0100

    not found page with navigate

--- sm-tunir-frontend/src/views/NotFound.jsx
+++ sm-tunir-frontend/src/views/NotFound.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react'
 import svg from "../assets/images/404.svg"
+import { useNavigate } from 'react-router'
 
 function NotFound() {
+  const navigate = useNavigate()
 
   return (
     <>
       <div className="cont-404">
         <img src={svg} alt="svg" />
-        <button>Back to Home</button>
+        <button onClick={()=>navigate('/')}>Back to Home</button>
       </div>
     </>
   )
