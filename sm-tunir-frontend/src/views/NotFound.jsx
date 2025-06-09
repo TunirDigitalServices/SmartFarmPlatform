384db62c5ee644e41a981820d710535932acd6a5
RanyaELOUNI <raniaelounidev@gmail.com>
2025-06-09 11:44:40 +0100

    not found page

--- sm-tunir-frontend/src/views/NotFound.jsx
+++ sm-tunir-frontend/src/views/NotFound.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect } from 'react'
+import svg from "../assets/images/404.svg"
 
 function NotFound() {
-   
+
   return (
-    <div>NotFound</div>
+    <>
+      <div className="cont-404">
+        <img src={svg} alt="svg" />
+        <button>Back to Home</button>
+      </div>
+    </>
   )
 }
 
